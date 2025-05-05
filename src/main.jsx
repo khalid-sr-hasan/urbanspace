442b267599344cdd22ad408bf031472f21e3dc13
Khalid Hasan <khalid.sr.hasan@gmail.com>
2025-05-06 03:53:25 +0600

    restore missing file

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
+        <App />
         <RouterProvider router={router} />
     </StrictMode>
 );
