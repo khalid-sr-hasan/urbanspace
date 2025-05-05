9942e8306f8cf11d15375d1270934e1741b4a7bd
Khalid Hasan <khalid.sr.hasan@gmail.com>
2025-05-06 03:54:11 +0600

    restore missing file

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <App />
-        <RouterProvider router={router} />
+        {/* <RouterProvider router={router} /> */}
     </StrictMode>
 );
